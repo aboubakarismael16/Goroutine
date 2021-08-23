aa61e5d57bbf9a86b83bcdf59ffb195b1e89ff13
aboubakar ismael <aboubakarismael16@gmail.com>
2021-08-23 08:05:18 +0800

    Update README.md

--- channel/lesson13.go
+++ channel/lesson13.go
@@ -6,21 +6,21 @@ import (
 )
 
 var b chan int
-var wg13 sync.WaitGroup
+var wg sync.WaitGroup
 
 func noBufChannel()  {
 	fmt.Println(b) // nil
 	b = make(chan int) // you need to initialize the channel
-	wg13.Add(1)
+	wg.Add(1)
 	go func() {
-		defer wg13.Done()
+		defer wg.Done()
 		x := <- b
 		fmt.Println("Received the sent value from b : ",x)
 	}()
 
 	b <- 10
 	fmt.Println("noBufChannel : 10 send to channel b ...")
-	wg13.Wait()
+	wg.Wait()
 }
 
 func bufChannel()  {
