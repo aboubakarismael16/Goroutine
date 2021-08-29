f1e9bb1cc84e02cb838d7ea598ee15f8d1953b81
aboubakar ismael <aboubakarismael16@gmail.com>
2021-08-29 15:23:53 +0800

    add new lesson in channel

---
+++ channel/lesson08.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	//ch1 := make(chan int)     // unbuffered
+	//fmt.Println(len(ch1), cap(ch1))  //0 0
+	//ch1 <- 10  // unbuffered channel, it's need to another goroutine to lock the block
+
+	ch2 := make(chan int, 5) // buffered , cap = 5
+	fmt.Println(len(ch2), cap(ch2))
+
+	for i := 0; i < 50; i++ {
+		ch2 <- i+10
+	}
+	//ch2 <- 10
+	//ch2 <- 20
+	//ch2 <- 30
+	//ch2 <- 40
+	//ch2 <- 50
+	fmt.Println("------------")
+	fmt.Println(len(ch2))
+
+}
--- main.go
+++ main.go
@@ -3,20 +3,14 @@ package main
 import "fmt"
 
 func main()  {
-	var ch1 chan bool
-	fmt.Println(ch1)
-	ch1 = make(chan bool)
-	fmt.Println(ch1)
+	ch1 := make(chan int, 5)
+	ch1 <- 10
+	fmt.Println(len(ch1), cap(ch1))
+	ch1 <- 20
+	ch1 <- 30
+	ch1 <- 40
+	ch1 <- 50
+	fmt.Println("------------")
+	fmt.Println(len(ch1))
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println("start to print value of i:", i)
-		}
-		ch1 <- true
-		fmt.Println("end to send")
-	}()
-
-	data := <- ch1
-	fmt.Println("data-->", data)
-	fmt.Println("main goroutine over")
 }
