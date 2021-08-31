dc675f5270f00ae410ee469d91e26bc0dc6529d3
aboubakar ismael <aboubakarismael16@gmail.com>
2021-08-31 10:16:49 +0800

    add new example in readme.md

--- main.go
+++ main.go
@@ -5,30 +5,20 @@ import (
 	"time"
 )
 
-func main()  {
-
-	//timer := time.NewTimer(1*time.Second)
-	//fmt.Printf("%T\n", timer)
-	//fmt.Println(time.Now())
-	//
-	//ch := timer.C
-	//fmt.Println(<-ch)
-
-	fmt.Println("--------------")
-
-	timer2 := time.NewTimer(5 * time.Second)
-
-	go func() {
-		 <- timer2.C
-
-		 fmt.Println("timer2 finished")
-	}()
-
-	time.Sleep(3*time.Second)
-	stop := timer2.Stop()
-
-	if stop {
-		fmt.Println("timer 2 stopped")
+func write(ch chan int)  {
+	for i := 0; i < 5; i++ {
+		ch <- i
+		fmt.Println("Successful wrote", i, "to ch")
 	}
+	close(ch)
+}
 
+func main()  {
+	ch := make(chan int, 2)
+	go write(ch)
+	time.Sleep(2*time.Millisecond)
+	for v := range ch {
+		fmt.Println("read value", v, "from ch")
+		time.Sleep(2*time.Millisecond)
+	}
 }
